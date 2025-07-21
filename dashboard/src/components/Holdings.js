39c2bb3f341c6ab518f2e4adfa8efc7ef6a95b89
sundar112 <kspsuraj44.ks@gmail.com>
2025-07-21 17:41:37 +0545

    implimented charts

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { VerticalGraph } from './VerticalGraph';
 
 const Holdings = () => {
 	const [allHoldings, setAllHoldings] = useState([]);
@@ -11,6 +12,19 @@ const Holdings = () => {
 			setAllHoldings(res.data);
 		});
 	}, []);
+
+	const labels = allHoldings.map((subArray) => subArray['name']);
+
+	const data = {
+		labels,
+		datasets: [
+			{
+				label: 'Stock Name',
+				data: allHoldings.map((stock) => stock.price),
+				backgroundColor: 'rgba(255, 99, 132, 0.5)',
+			},
+		],
+	};
 
 	return (
 		<>
@@ -69,6 +83,8 @@ const Holdings = () => {
 					<p>P&L</p>
 				</div>
 			</div>
+
+			<VerticalGraph data={data} />
 		</>
 	);
 };
